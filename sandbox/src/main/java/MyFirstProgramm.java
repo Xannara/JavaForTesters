e332cb597488a946f1c8c9e29bb32ccd34b36777
Xannara <30448299+Xannara@users.noreply.github.com>
2020-02-09 19:36:01 +0300

    Добавлены функции вычисления площади квадрата и прямоугольника

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -1,7 +1,27 @@
 public class MyFirstProgramm {
 
    public static void main(String[] args) {
+       hello("world");
+       hello("user");
 
-      System.out.println("Hello, world!");
+       double len = 5;
+       System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+       double a = 4;
+       double b = 5;
+       System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+   }
+
+   public static void hello(String somebody) {
+       System.out.println("Hello, " + somebody + "!");
+   }
+
+   public static double area (double l) {
+       return l * l;
+   }
+
+   public static double area (double a, double b) {
+       return a * b;
    }
 }
