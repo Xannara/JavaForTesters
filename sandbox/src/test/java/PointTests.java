39ac94facc5628a211ca1c4a62a8a05757035f30
Xannara <30448299+Xannara@users.noreply.github.com>
2020-02-11 00:18:08 +0300

    Еще один тест

--- sandbox/src/test/java/PointTests.java
+++ sandbox/src/test/java/PointTests.java
@@ -5,8 +5,8 @@ public class PointTests {
     @Test
     public void testDistance() {
 
-        Point p1 = new Point(4, 9);
-        Point p2 = new Point(10, 15);
-        Assert.assertEquals(p1.distance(p2), 8.48528137423857);
+        Point p1 = new Point(1.5, 5.9);
+        Point p2 = new Point(9.1, 11.7);
+        Assert.assertEquals(p1.distance(p2), 9.560334722173694);
     }
 }
