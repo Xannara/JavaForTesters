62e880db85e98f8bd36b91a5158fa3d5709700bc
Xannara <30448299+Xannara@users.noreply.github.com>
2020-02-10 23:51:27 +0300

    Исправлен метод, убраны комментарии

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,33 +1,15 @@
 public class Point {
 
-    public double x1;
-    public double y1;
-    public double x2;
-    public double y2;
+    public double x;
+    public double y;
 
-    public Point (double x1, double y1, double x2, double y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+    public Point (double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-   /* double distance(double x, double y) {
-        double px = this.x - x;
-        double py = this.y - y;
-        return  Math.sqrt(px*px + py*py);
-    }*/
-
-    /* public static double distanceP(Point p1, Point p2) {
-        return  Math.sqrt( Math.pow((p.x2 - p.x1), 2) + Math.pow((p.y2 - p.y1), 2));
-    }
-
-    public double distance(Point p) {
-        return distanceP();
-    }*/
-
-    public double distance() {
-        return  Math.sqrt(Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2));
+    public double distance(Point p2) {
+        return  Math.sqrt( Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
     }
 
 }
--- sandbox/src/main/java/PointProgram.java
+++ sandbox/src/main/java/PointProgram.java
@@ -1,27 +1,15 @@
 public class PointProgram {
     public static void main(String[] args) {
 
-        Point p = new Point(4, 6, 10, 15);
-        //Point p1 = new Point(3, 5);
-   //     p1.x = 3;
-   //     p1.y = 5;
+        Point p1 = new Point(3, 5);
+        p1.x = 5;
+        p1.y = 7;
 
-      //  Point p2 = new Point(10, 15);
-   //     p2.x = 10;
-   //     p2.y = 15;
+        Point p2 = new Point(10, 15);
+        p2.x = 15;
+        p2.y = 21;
 
-        System.out.println("Расстояние между двумя точками с координатами " + "(" + p.x1 + ", " + p.y1 + ")" + " и "
-                + "(" + p.x2 + ", " + p.y2 + ")" +" = " + p.distance());
+        System.out.println("Расстояние между двумя точками с координатами " + "(" + p1.x + ", " + p1.y + ")" + " и "
+                + "(" + p2.x + ", " + p2.y + ")" +" = " + p1.distance(p2));
     }
-
-   /*  System.out.println("Расстояние между двумя точками с координатами " + "(" + p1.x + ", " + p1.y + ")" + " и "
-            + "(" + p2.x + ", " + p2.y + ")" +" = " + p1.distance(p2));*/
-
-     /* public static double distanceP(Point p1, Point p2) {
-        return  Math.sqrt( Math.pow((p.x2 - p.x1), 2) + Math.pow((p.y2 - p.y1), 2));
-    }
-
-   /*public static double distance(Point p) {
-        return  Math.sqrt( Math.pow((p.x2 - p.x1), 2) + Math.pow((p.y2 - p.y1), 2));
-    }*/
 }
