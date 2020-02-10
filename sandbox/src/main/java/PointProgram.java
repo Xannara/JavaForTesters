4287caa15d195dd49eb7685f0459faca99d0440f
Xannara <30448299+Xannara@users.noreply.github.com>
2020-02-11 00:08:04 +0300

    Исправлена передача координат

--- sandbox/src/main/java/PointProgram.java
+++ sandbox/src/main/java/PointProgram.java
@@ -1,13 +1,9 @@
 public class PointProgram {
     public static void main(String[] args) {
 
-        Point p1 = new Point(3, 5);
-        p1.x = 5;
-        p1.y = 7;
+        Point p1 = new Point(4, 9);
 
         Point p2 = new Point(10, 15);
-        p2.x = 15;
-        p2.y = 21;
 
         System.out.println("Расстояние между двумя точками с координатами " + "(" + p1.x + ", " + p1.y + ")" + " и "
                 + "(" + p2.x + ", " + p2.y + ")" +" = " + p1.distance(p2));
