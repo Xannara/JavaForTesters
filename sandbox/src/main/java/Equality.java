9c6021dbd1291c8997b0aa9931f72624ca07c7dd
Xannara <30448299+Xannara@users.noreply.github.com>
2020-02-25 23:48:37 +0300

    Сравнение строк

---
+++ sandbox/src/main/java/Equality.java
@@ -0,0 +1,9 @@
+public class Equality {
+    public static void main(String[] args) {
+        String s1 = "chrome";
+        String s2 = new String(s1);
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+    }
+}
