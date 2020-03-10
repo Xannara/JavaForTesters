3fb1714b65c7b446edcadc838e9b9bfe8dd9d28d
Xannara <30448299+Xannara@users.noreply.github.com>
2020-03-10 23:53:12 +0300

    Выбор элемента списка по порядковому номеру

--- addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
@@ -9,12 +9,12 @@ public class GroupModificationTests extends TestBase {
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
         int before = app.getGroupHelper().getGroupCount();
-        if (! app.getGroupHelper().isThereAGroup) {
+       /* if (! app.getGroupHelper().isThereAGroup) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-        }
-        app.getGroupHelper().selectGroup();
+        }*/
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test10", "test20", "test30"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test15", "test20", "test30"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         int after = app.getGroupHelper().getGroupCount();
