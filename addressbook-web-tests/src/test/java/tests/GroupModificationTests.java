674f72279bb90c4db39016013185031845e8fd22
Xannara <30448299+Xannara@users.noreply.github.com>
2020-03-12 00:04:45 +0300

    Формирование коллекции

--- addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
@@ -4,20 +4,22 @@ import model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
        /* if (! app.getGroupHelper().isThereAGroup) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }*/
-        app.getGroupHelper().selectGroup(before - 1);
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test15", "test20", "test30"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test10", "test20", "test30"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
