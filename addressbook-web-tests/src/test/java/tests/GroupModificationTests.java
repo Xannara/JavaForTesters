3f3768c0ff9d06ab3aa3b816af44b996da10512d
Xannara <30448299+Xannara@users.noreply.github.com>
2020-03-12 23:41:12 +0300

    Множества: неупорядоченные коллекции, сравнение в модификации групп

--- addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -16,10 +17,16 @@ public class GroupModificationTests extends TestBase {
         }*/
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test10", "test20", "test30"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test15", "test20", "test30");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
     }
 }
