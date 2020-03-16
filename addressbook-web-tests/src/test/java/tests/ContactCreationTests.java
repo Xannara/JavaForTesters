9118c290b12ac522820624aec76d827bde58d548
Xannara <30448299+Xannara@users.noreply.github.com>
2020-03-16 03:11:13 +0300

    Вычисление максимального идентификатора при помощи лямбда-выражения

--- addressbook-web-tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,18 +14,12 @@ public class ContactCreationTests extends TestBase {
   public void testAddName() throws Exception {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().gotoAddNewPage();
-    ContactData contact = new ContactData("Lena", "Lenina", "Moscow", "88007776655", "lena@mail.com", "test1");
+    ContactData contact = new ContactData("Lina", "Linina", "Moscow", "88007776655", "lena@mail.com", "test1");
     app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(),before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
