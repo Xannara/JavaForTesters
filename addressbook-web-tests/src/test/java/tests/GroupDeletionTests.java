ef75a0e80a164a12dcbc2d98c728fbe017f56795
Xannara <30448299+Xannara@users.noreply.github.com>
2020-03-18 00:36:27 +0300

    Выделены предусловия создания групп

--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -2,19 +2,24 @@ package tests;
 
 import model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.getNavigationHelper().gotoGroupPage();
+    /* if (! app.getGroupHelper().isThereAGroup) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }*/
+  }
+
   @Test
   public void testGroupDeletion() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-   /* if (! app.getGroupHelper().isThereAGroup) {
-        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    }*/
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
