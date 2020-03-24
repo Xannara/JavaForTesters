022fdc873edc440dc40b91407ee0042d3f44da14
Xannara <30448299+Xannara@users.noreply.github.com>
2020-03-24 23:58:17 +0300

    В тест проверки контактов добавлено предусловие

--- addressbook-web-tests/src/test/java/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactPhoneTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import model.ContactData;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
@@ -11,7 +12,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTests extends TestBase {
 
-
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.goTo().homePage();
+        if (app.contact().allContact().size() == 0) {
+            app.contact().gotoAddNewPage();
+            app.contact().create(new ContactData()
+                    .withFirstname("Lena").withLastname("Lenina").withAddress("Moscow")
+                    .withHomePhone("88007776655").withMobile("89167776655").withWork("4722555555")
+                    .withEmail("lena@mail.com").withGroup("test10"));
+        }
+    }
 
     @Test
     public void testContactPhones() {
