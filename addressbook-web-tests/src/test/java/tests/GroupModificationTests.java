425265a4e71e38451d47dbd04bc2833ea3f75913
Xannara <30448299+Xannara@users.noreply.github.com>
2020-04-06 00:58:10 +0400

    Срвнение данных, полученных из БД (модификация групп)

--- addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
@@ -12,21 +12,22 @@ public class GroupModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().groupPage();
-          if ( app.group().allGroup().size() == 0) {
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
             app.group().create(new GroupData().withName("test1"));
-          }
+        }
     }
 
     @Test
     public void testGroupModification() {
-        Groups before = app.group().allGroup();
+        Groups before = app.db().groups();
         GroupData modifiedGroup = before.iterator().next();
         GroupData group = new GroupData()
                 .withId(modifiedGroup.getId()).withName("test15").withHeader("test20").withFooter("test30");
+        app.goTo().groupPage();
         app.group().modify(group);
         assertThat(app.group().count(), equalTo(before.size()));
-        Groups after = app.group().allGroup();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before.withOut(modifiedGroup).withAdded(group)));
     }
 }
